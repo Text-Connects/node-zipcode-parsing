f82fe6efd78ad197748232d2c0b91482c52fecfc
Elva Rothman <erothman@tulane.edu>
2023-10-25 14:00:17 -0400

    downloaded json data

--- server.js
+++ server.js
@@ -4,4 +4,22 @@ const fetch = require("node-fetch")
 const app = express()
 const port = 3000
 
+const API_URL = "http://documentation-resources.opendatasoft.com"
+
 app.use(express.json())
+
+app.get(`/api/explore/v2.1/:${country}/:${zipcode}`, async (req, res) => {
+    try{
+        const data = req.params.country + req.params.zipcode
+        const apiResponse = await fetch(API_URL)
+        const jsonData = await apiResponse.json()
+        res.json(data, jsonData)
+    } catch(error) {
+        console.error(`Couldn't fetch from API: ${error}`)
+        res.status(500).json({ error: `internal server error`})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
