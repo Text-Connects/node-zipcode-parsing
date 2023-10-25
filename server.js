4f5e8e3841c58a498604b66b4b47246c8e2b8aa0
Elva Rothman <erothman@tulane.edu>
2023-10-25 14:43:10 -0400

    got server.js to output data

--- server.js
+++ server.js
@@ -1,25 +1,46 @@
 const express = require("express")
-const fetch = require("node-fetch")
+const fs = require("fs")
+const path = require("path")
 
 const app = express()
 const port = 3000
 
-const API_URL = "http://documentation-resources.opendatasoft.com"
+const dataPath = path.join(__dirname, "data-zipcodes.json")
+const zipcodeData = JSON.parse(fs.readFileSync(dataPath, "utf-8"))
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get(`/api/explore/v2.1/:${country}/:${zipcode}`, async (req, res) => {
-    try{
-        const data = req.params.country + req.params.zipcode
-        const apiResponse = await fetch(API_URL)
-        const jsonData = await apiResponse.json()
-        res.json(data, jsonData)
-    } catch(error) {
-        console.error(`Couldn't fetch from API: ${error}`)
-        res.status(500).json({ error: `internal server error`})
+app.get(`/api/:state/:zipcode`, (req, res) => {
+    const state = req.params.state
+    const zipcode = req.params.zipcode
+
+    const matchFound = zipcodeData.find(data => data.stusps_code === state && data.zip_code === zipcode)
+
+    if (matchFound) {
+        res.json(matchFound)
+    } else {
+        res.status(404).json({ error: `Not found :(`})
     }
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
+
+
+// const API_URL = "http://documentation-resources.opendatasoft.com"
+
+// app.use(express.json())
+
+// app.get(`/api/:${state}/:${zipcode}`, async (req, res) => {
+//     try{
+//         const data = req.params.state + req.params.zipcode
+//         const apiResponse = await fetch(API_URL)
+//         const jsonData = await apiResponse.json()
+//         res.json(data, jsonData)
+//     } catch(error) {
+//         console.error(`Couldn't fetch from API: ${error}`)
+//         res.status(500).json({ error: `internal server error`})
+//     }
+// })
